5b4ce5df846f5f7e6d602b75a231995489327e4f
Pihu Gupta <pihugupta0501@gmail.com>
2024-04-16 14:45:01 -0700

    Define instance variables @products and @category with the counts of each from the database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV["PASSWORD"]
+
+
   def show
+    @products = Product.count
+    @category = Category.count
   end
 end
